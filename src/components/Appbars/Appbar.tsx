98411364c7a71a74bb9b4ce925e84eedb7e9c522
anoopa413 <anoopa413@gail.com>
2024-08-21 22:55:08 +0530

    card styling

--- src/components/Appbars/Appbar.tsx
+++ src/components/Appbars/Appbar.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Loader from '../Loader/Loader'
+import Card from '../Cards/Card'
 
 const Appbar = () => {
   return (
@@ -6,6 +8,8 @@ const Appbar = () => {
     <>
     <div className='w-full h-200px  '>
         Heloo
+        <Card/>
+
     </div>
     
     </>
---
+++ src/components/Cards/Card.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './card.css'
+
+const Card = ({}) => {
+  return (
+    <>
+
+    <div className=' w-64 min-w-[250px] max-w-[300px] h-[320px]  border border-customBg-bgcoldark/10 flex flex-col justify-center items-center relative'>
+
+    <div className='quartercircle' />
+            Hlooooo
+    <div>
+
+    </div>
+
+    </div>
+    </>
+
+  )
+}
+
+export default Card
